e1d49b699227a5b49b2a10d895a00cecc5bd9fee
Elia <reutlinger.elia@gmail.com>
2021-08-02 07:47:01 +0200

    Update schedule.js

--- src/module/gamebot/schedule.js
+++ src/module/gamebot/schedule.js
@@ -27,17 +27,9 @@ async function initSchedules() {
       type: 'messages',
       values: [{ scope: sGlobal, content: await bms.get('schedule.restart.3', 'def') }]
     },
-    '11:55': {
+    '11:58': {
       type: 'messages',
-      values: [{ scope: sGlobal, content: await bms.get('schedule.restart.1', 'def') }]
-    },
-    '11:57': {
-      type: 'messages',
-      values: [{ scope: sGlobal, content: await bms.get('schedule.restart.2', 'def') }]
-    },
-    '11:59': {
-      type: 'messages',
-      values: [{ scope: sGlobal, content: await bms.get('schedule.restart.3', 'def') }]
+      values: [{ scope: sGlobal, content: '(Restart at 12:00 will be skipped)' }]
     },
     '17:55': {
       type: 'messages',
@@ -56,6 +48,18 @@ async function initSchedules() {
       values: [{ scope: sGlobal, content: '(Restart at 00:00 will be skipped)' }]
     }
     /*
+    '11:55': {
+      type: 'messages',
+      values: [{ scope: sGlobal, content: await bms.get('schedule.restart.1', 'def') }]
+    },
+    '11:57': {
+      type: 'messages',
+      values: [{ scope: sGlobal, content: await bms.get('schedule.restart.2', 'def') }]
+    },
+    '11:59': {
+      type: 'messages',
+      values: [{ scope: sGlobal, content: await bms.get('schedule.restart.3', 'def') }]
+    },
     '23:55': {
       type: 'messages',
       values: [{ scope: sGlobal, content: await bms.get('schedule.restart1 }]
